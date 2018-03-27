b36c58c14befea2ed0ea3949877b728d86b53855
achou11 <chouandrewo@gmail.com>
2018-03-27 13:47:36 -0500

    Initial upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const colorInput = document.getElementById('color-input')
+const colorButton = document.getElementById('color-btn')
+const resetButton = document.getElementById('reset-btn')
+const sourceUrl= document.getElementById('source-url')
+
+function changeByKey(event) {
+  // 13 = enter key
+  // 27 = esc key
+  if (event.keyCode === 13) {
+    changeColor()
+  } else if (event.keyCode === 27) {
+    resetColor()
+  }
+}
+
+function changeColor() {
+  resetButton.style.borderColor = colorInput.value
+  colorButton.style.backgroundColor = colorInput.value
+}
+
+function resetColor() {
+  colorInput.value = ""
+  colorButton.style.backgroundColor = "#ffffff"
+  resetButton.style.borderColor = "#000000"
+}
+
+function createSourceLink() {
+  const isDat = window.DatArchive && window.location.origin.startsWith('dat:')
+  const url = isDat
+    ? `beaker://library/${window.location}`
+    : 'https://github.com/achou11/color-button'
+
+  sourceUrl.href = url
+}
+
+window.addEventListener('keydown', changeByKey)
+colorButton.addEventListener('click', changeColor)
+resetButton.addEventListener('click', resetColor)
+document.addEventListener('DOMContentLoaded', createSourceLink)
+
